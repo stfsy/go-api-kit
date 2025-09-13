3b2c1e6b526aa37bc4f376e87485eddea48a324f
Stefan Pfaffel <s.pfaffel@gmail.com>
2025-09-13 22:16:06 +0200

    feat: update error handling, load server config
    Signed-off-by: Stefan Pfaffel <s.pfaffel@gmail.com>

--- config/config.go
+++ config/config.go
@@ -4,10 +4,12 @@ import (
 	"github.com/kelseyhightower/envconfig"
 )
 
+// AppConfig holds application-level configuration.
 type AppConfig struct {
 	Env string `default:"production"`
 }
 
+// ServerConfig holds server-related configuration.
 type ServerConfig struct {
 	MaxBodySize  int `default:"10485760" split_words:"true"`
 	ReadTimeout  int `default:"10" split_words:"true"`  // seconds
@@ -15,10 +17,12 @@ type ServerConfig struct {
 	IdleTimeout  int `default:"620" split_words:"true"` // seconds
 }
 
+// ContainerConfig holds container-specific configuration.
 type ContainerConfig struct {
 	Port string `default:"8080"`
 }
 
+// Configuration aggregates all config sections.
 type Configuration struct {
 	AppConfig
 	ServerConfig
@@ -27,31 +31,53 @@ type Configuration struct {
 
 var c *Configuration
 
-func Load() {
+// Load loads configuration from environment variables.
+// Returns an error if loading fails.
+func Load() error {
 	var a AppConfig
-	envconfig.MustProcess("API_KIT", &a)
+	err := envconfig.Process("API_KIT", &a)
+	if err != nil {
+		return err
+	}
 
-	var s ContainerConfig
-	envconfig.MustProcess("", &s)
+	var s ServerConfig
+	err = envconfig.Process("API_KIT", &s)
+	if err != nil {
+		return err
+	}
+
+	var ct ContainerConfig
+	err = envconfig.Process("", &ct)
+	if err != nil {
+		return err
+	}
 
 	c = &Configuration{
 		AppConfig:       a,
-		ContainerConfig: s,
+		ServerConfig:    s,
+		ContainerConfig: ct,
 	}
+	return nil
 }
 
+// Get returns the loaded configuration, loading it if necessary.
+// Panics if loading fails.
 func Get() Configuration {
 	if c == nil {
-		Load()
+		err := Load()
+		if err != nil {
+			panic(err)
+		}
 	}
-
 	return *c
 }
 
+// reset clears the loaded configuration (for testing).
 func reset() {
 	c = nil
 }
 
+// IsProduction returns true if the environment is production.
 func IsProduction() bool {
 	return Get().Env == "production"
 }
