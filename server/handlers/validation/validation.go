de9824a530808debec88d2147078a36443f05d15
Stefan Pfaffel <s.pfaffel@gmail.com>
2025-09-24 21:14:44 +0200

    chore: do not append uppercase field name to error message
    Signed-off-by: Stefan Pfaffel <s.pfaffel@gmail.com>

--- server/handlers/validation/validation.go
+++ server/handlers/validation/validation.go
@@ -54,97 +54,97 @@ func getErrorMessage(fe validator.FieldError) string {
 	switch fe.Tag() {
 	// Required and length
 	case "required":
-		return fmt.Sprintf("%s must not be undefined", fe.Field())
+		return "must not be undefined"
 	case "min":
-		return fmt.Sprintf("%s must be at least %s characters long", fe.Field(), fe.Param())
+		return fmt.Sprintf("must be at least %s characters long", fe.Param())
 	case "max":
-		return fmt.Sprintf("%s must be at most %s characters long", fe.Field(), fe.Param())
+		return fmt.Sprintf("must be at most %s characters long", fe.Param())
 	case "len":
-		return fmt.Sprintf("%s must be exactly %s characters long", fe.Field(), fe.Param())
+		return fmt.Sprintf("must be exactly %s characters long", fe.Param())
 
 	// Comparisons
 	case "eq":
-		return fmt.Sprintf("%s must be equal to %s", fe.Field(), fe.Param())
+		return fmt.Sprintf("must be equal to %s", fe.Param())
 	case "ne":
-		return fmt.Sprintf("%s must not be equal to %s", fe.Field(), fe.Param())
+		return fmt.Sprintf("must not be equal to %s", fe.Param())
 	case "lt":
-		return fmt.Sprintf("%s must be less than %s", fe.Field(), fe.Param())
+		return fmt.Sprintf("must be less than %s", fe.Param())
 	case "lte":
-		return fmt.Sprintf("%s must be less than or equal to %s", fe.Field(), fe.Param())
+		return fmt.Sprintf("must be less than or equal to %s", fe.Param())
 	case "gt":
-		return fmt.Sprintf("%s must be greater than %s", fe.Field(), fe.Param())
+		return fmt.Sprintf("must be greater than %s", fe.Param())
 	case "gte":
-		return fmt.Sprintf("%s must be greater than or equal to %s", fe.Field(), fe.Param())
+		return fmt.Sprintf("must be greater than or equal to %s", fe.Param())
 	case "eqfield":
-		return fmt.Sprintf("%s must be equal to %s", fe.Field(), fe.Param())
+		return fmt.Sprintf("must be equal to %s", fe.Param())
 	case "nefield":
-		return fmt.Sprintf("%s must not be equal to %s", fe.Field(), fe.Param())
+		return fmt.Sprintf("must not be equal to %s", fe.Param())
 	case "gtfield":
-		return fmt.Sprintf("%s must be greater than %s", fe.Field(), fe.Param())
+		return fmt.Sprintf("must be greater than %s", fe.Param())
 	case "gtefield":
-		return fmt.Sprintf("%s must be greater than or equal to %s", fe.Field(), fe.Param())
+		return fmt.Sprintf("must be greater than or equal to %s", fe.Param())
 	case "ltfield":
-		return fmt.Sprintf("%s must be less than %s", fe.Field(), fe.Param())
+		return fmt.Sprintf("must be less than %s", fe.Param())
 	case "ltefield":
-		return fmt.Sprintf("%s must be less than or equal to %s", fe.Field(), fe.Param())
+		return fmt.Sprintf("must be less than or equal to %s", fe.Param())
 	case "oneof":
-		return fmt.Sprintf("%s must be one of [%s]", fe.Field(), fe.Param())
+		return fmt.Sprintf("must be one of [%s]", fe.Param())
 
 	// String types
 	case "alpha":
-		return fmt.Sprintf("%s must contain only alphabetic characters", fe.Field())
+		return "must contain only alphabetic characters"
 	case "alphanum":
-		return fmt.Sprintf("%s must contain only alphanumeric characters", fe.Field())
+		return "must contain only alphanumeric characters"
 	case "alphanumunicode":
-		return fmt.Sprintf("%s must contain only alphanumeric characters and spaces", fe.Field())
+		return "must contain only alphanumeric characters and spaces"
 	case "email":
-		return fmt.Sprintf("%s must be a valid email address", fe.Field())
+		return "must be a valid email address"
 
 	// Others
 	case "url":
-		return fmt.Sprintf("%s must be a valid URL", fe.Field())
+		return "must be a valid URL"
 	case "uri":
-		return fmt.Sprintf("%s must be a valid URI", fe.Field())
+		return "must be a valid URI"
 	case "uuid":
-		return fmt.Sprintf("%s must be a valid UUID", fe.Field())
+		return "must be a valid UUID"
 	case "uuid3":
-		return fmt.Sprintf("%s must be a valid UUIDv3", fe.Field())
+		return "must be a valid UUIDv3"
 	case "uuid4":
-		return fmt.Sprintf("%s must be a valid UUIDv4", fe.Field())
+		return "must be a valid UUIDv4"
 	case "uuid5":
-		return fmt.Sprintf("%s must be a valid UUIDv5", fe.Field())
+		return "must be a valid UUIDv5"
 	case "isbn":
-		return fmt.Sprintf("%s must be a valid ISBN", fe.Field())
+		return "must be a valid ISBN"
 	case "isbn10":
-		return fmt.Sprintf("%s must be a valid ISBN-10", fe.Field())
+		return "must be a valid ISBN-10"
 	case "isbn13":
-		return fmt.Sprintf("%s must be a valid ISBN-13", fe.Field())
+		return "must be a valid ISBN-13"
 	case "contains":
-		return fmt.Sprintf("%s must contain '%s'", fe.Field(), fe.Param())
+		return fmt.Sprintf("must contain '%s'", fe.Param())
 	case "excludes":
-		return fmt.Sprintf("%s must not contain '%s'", fe.Field(), fe.Param())
+		return fmt.Sprintf("must not contain '%s'", fe.Param())
 	case "startswith":
-		return fmt.Sprintf("%s must start with '%s'", fe.Field(), fe.Param())
+		return fmt.Sprintf("must start with '%s'", fe.Param())
 	case "endswith":
-		return fmt.Sprintf("%s must end with '%s'", fe.Field(), fe.Param())
+		return fmt.Sprintf("must end with '%s'", fe.Param())
 	case "ip":
-		return fmt.Sprintf("%s must be a valid IP address", fe.Field())
+		return "must be a valid IP address"
 	case "ipv4":
-		return fmt.Sprintf("%s must be a valid IPv4 address", fe.Field())
+		return "must be a valid IPv4 address"
 	case "ipv6":
-		return fmt.Sprintf("%s must be a valid IPv6 address", fe.Field())
+		return "must be a valid IPv6 address"
 	case "mac":
-		return fmt.Sprintf("%s must be a valid MAC address", fe.Field())
+		return "must be a valid MAC address"
 	case "cidr":
-		return fmt.Sprintf("%s must be a valid CIDR notation", fe.Field())
+		return "must be a valid CIDR notation"
 	case "cidrv4":
-		return fmt.Sprintf("%s must be a valid CIDR notation (IPv4)", fe.Field())
+		return "must be a valid CIDR notation (IPv4)"
 	case "cidrv6":
-		return fmt.Sprintf("%s must be a valid CIDR notation (IPv6)", fe.Field())
+		return "must be a valid CIDR notation (IPv6)"
 	case "dive":
-		return fmt.Sprintf("%s must have valid items only", fe.Field())
+		return "must have valid items only"
 
 	default:
-		return fmt.Sprintf("%s is invalid", fe.Field())
+		return "is invalid"
 	}
 }
