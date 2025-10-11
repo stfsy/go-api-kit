b2ed4216311b909d0f588a9466e31e21a4ac4306
Stefan Pfaffel <s.pfaffel@gmail.com>
2025-10-11 20:51:03 +0200

    chore(ci): use latest golangci-lint image

--- lint.sh
+++ lint.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 MSYS_NO_PATHCONV=1 docker run --rm \
 --mount type=bind,src=.,dst=/app \
--w /app golangci/golangci-lint:v2.0 golangci-lint \
+-w /app golangci/golangci-lint:latest-alpine golangci-lint \
 run -v
