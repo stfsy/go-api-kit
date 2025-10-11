bad05bb0c80c9a9c54da890648b6804660a020f2
Stefan Pfaffel <s.pfaffel@gmail.com>
2025-10-11 16:31:08 +0200

    fix: fix body detection for http delete
    Signed-off-by: Stefan Pfaffel <s.pfaffel@gmail.com>

--- server/handlers/validating_handler.go
+++ server/handlers/validating_handler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/stfsy/go-api-kit/server/handlers/validation"
 )
@@ -10,9 +11,22 @@ import (
 func ValidatingHandler[T any](handler func(http.ResponseWriter, *http.Request, *T)) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		method := r.Method
-		contentLength := r.ContentLength
-		// For methods that typically don't have a body, just call the handler with Nil
-		if (method == http.MethodPost || method == http.MethodPut || method == http.MethodPatch || method == http.MethodDelete) && contentLength > 0 {
+		hasBody := false
+		switch method {
+		case http.MethodPost, http.MethodPut, http.MethodPatch:
+			hasBody = true
+		case http.MethodDelete:
+			if r.ContentLength > 0 {
+				hasBody = true
+			} else {
+				te := strings.ToLower(strings.TrimSpace(r.Header.Get("Transfer-Encoding")))
+				if strings.Contains(te, "chunked") {
+					hasBody = true
+				}
+			}
+		}
+
+		if hasBody {
 			var body T
 			decoder := json.NewDecoder(r.Body)
 			decoder.DisallowUnknownFields()
